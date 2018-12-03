e8f964b82f30e362e80c560e4cdefe5c34c6939f
vimal <vimalkumar2020@gmail.com>
2018-12-04 01:39:30 +0530

    git commit

---
+++ IIHT/WEBAPI/ApiSample/Controllers/SupplierController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using DataAccess;
+
+namespace ApiSample.Controllers
+{
+    public class SupplierController : ApiController
+    {
+        private PODbEntities db = new PODbEntities();
+
+        // GET: api/Supplier
+        public IQueryable<SUPPLIER> GetSUPPLIERs()
+        {
+            return db.SUPPLIERs;
+        }
+
+        // GET: api/Supplier/5
+        [ResponseType(typeof(SUPPLIER))]
+        public IHttpActionResult GetSUPPLIER(string id)
+        {
+            SUPPLIER sUPPLIER = db.SUPPLIERs.Find(id);
+            if (sUPPLIER == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(sUPPLIER);
+        }
+
+        // PUT: api/Supplier/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutSUPPLIER(string id, SUPPLIER sUPPLIER)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != sUPPLIER.SUPLNO)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(sUPPLIER).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SUPPLIERExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Supplier
+        [ResponseType(typeof(SUPPLIER))]
+        public IHttpActionResult PostSUPPLIER(SUPPLIER sUPPLIER)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.SUPPLIERs.Add(sUPPLIER);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (SUPPLIERExists(sUPPLIER.SUPLNO))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = sUPPLIER.SUPLNO }, sUPPLIER);
+        }
+
+        // DELETE: api/Supplier/5
+        [ResponseType(typeof(SUPPLIER))]
+        public IHttpActionResult DeleteSUPPLIER(string id)
+        {
+            SUPPLIER sUPPLIER = db.SUPPLIERs.Find(id);
+            if (sUPPLIER == null)
+            {
+                return NotFound();
+            }
+
+            db.SUPPLIERs.Remove(sUPPLIER);
+            db.SaveChanges();
+
+            return Ok(sUPPLIER);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SUPPLIERExists(string id)
+        {
+            return db.SUPPLIERs.Count(e => e.SUPLNO == id) > 0;
+        }
+    }
+}
